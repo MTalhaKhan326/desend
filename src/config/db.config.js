011d04481150e42fa036d2bd516e4d67f44e00e7
Talhakhan326 <muhammadtalhakhan326@gmail.com>
2023-01-13 14:19:40 +0500

    update ssl option

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -16,7 +16,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
-        ssl: false
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
     }
 })
 
